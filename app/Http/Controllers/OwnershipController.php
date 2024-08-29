efb2169cb9dd721e7a11c316b95dece38cc5990b
mutiara-azzahra <my26recovery@gmail.com>
2024-08-29 16:06:24 +0800

    ownership kapal

--- app/Http/Controllers/OwnershipController.php
+++ app/Http/Controllers/OwnershipController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Ownership;
 
 class OwnershipController extends Controller
 {
-    //
+    public function index(){
+
+        $data = Ownership::paginate(20);
+        
+        return view('ownership.index', compact('data'));
+    }
 }
